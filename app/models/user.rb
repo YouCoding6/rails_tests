0b5d645797aadc73919ed49b803d448c63da2547
YoucefKhalainou <youcef_khalainou@yahoo.fr>
2021-10-11 09:05:09 +0200

    add validations and full_name method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :username, presence: true
+    validates :username, presence: true, length: { minimum: 3 }
     has_many :books 
+
+    def full_name
+        return [first_name, last_name].compact.join(' ')
+    end
 end
